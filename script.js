46d2e51e65b01c1ca995a8f0557fd5aae7e4ffc8
Damian Świerad <damian.swierad@gmail.com>
2015-08-31 20:07:10 +0200

    More PhoneGap approach try.

--- script.js
+++ script.js
@@ -1,68 +1,110 @@
-function error(message)
-{
-    $('#error').html(message);
-}
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        $('button').click(function() {
+            error('');
+            $('#hourly').html('');
+            app.calculate();
+        });
+    },
+    // Update DOM on a Received Event
+    // receivedEvent: function(id) {
+    //     var parentElement = document.getElementById(id);
+    //     var listeningElement = parentElement.querySelector('.listening');
+    //     var receivedElement = parentElement.querySelector('.received');
 
-function getHolidaysPaycheck(days)
-{
-    var rate = 62;
+    //     listeningElement.setAttribute('style', 'display:none;');
+    //     receivedElement.setAttribute('style', 'display:block;');
 
-    return days * rate;
-}
+    //     console.log('Received Event: ' + id);
+    // }
 
-function getWeekendPaycheck(days)
-{
-    var rate = 55;
+    error: function(message)
+    {
+        $('#error').html(message);
+    },
 
-    return days * rate;
-}
+    getHolidaysPaycheck: function(days)
+    {
+        var rate = 62;
 
-function calculate()
-{
-    var sum = parseInt($('input[name=sum]').val());
+        return days * rate;
+    },
 
-    var paycheck = {
-        a: 0,
-        s: 0,
-        j: 0
+    getWeekendPaycheck: function(days)
+    {
+        var rate = 55;
+
+        return days * rate;
+    },
+
+    calculate: function()
+    {
+        var sum = parseInt($('input[name=sum]').val());
+
+        var paycheck = {
+            a: 0,
+            s: 0,
+            j: 0
+        }
+
+        if (sum <= 0 || isNaN(sum)) {
+            error('Podaj sumę zarobioną przez zespół');
+        }
+
+        var workdaysSum = 0;
+        $('[name^=workdays]').each(function(index, field) {
+            workdaysSum += parseInt($(field).val());
+        });
+
+        if (workdaysSum > 0) {
+            var rate = sum / workdaysSum;
+
+            $('#hourly').html(Math.round((rate / 8) * 100) / 100);
+
+            paycheck.a = rate * parseInt($('[name=workdays\\[a\\]').val());
+            paycheck.s = rate * parseInt($('[name=workdays\\[s\\]]').val());
+            paycheck.j = rate * parseInt($('[name=workdays\\[j\\]]').val());
+        }
+
+        // Urlop
+        paycheck.a += app.getHolidaysPaycheck(parseInt($('[name=holidays\\[a\\]]').val()));
+        paycheck.s += app.getHolidaysPaycheck(parseInt($('[name=holidays\\[s\\]]').val()));
+        paycheck.j += app.getHolidaysPaycheck(parseInt($('[name=holidays\\[j\\]]').val()));
+
+        // Soboty
+        paycheck.a += app.getWeekendPaycheck(parseInt($('[name=weekend\\[a\\]]').val()));
+        paycheck.s += app.getWeekendPaycheck(parseInt($('[name=weekend\\[s\\]]').val()));
+        paycheck.j += app.getWeekendPaycheck(parseInt($('[name=weekend\\[j\\]]').val()));
+
+        $('[name=paycheck\\[a\\]]').val(Math.round(paycheck.a * 100) / 100, 2);
+        $('[name=paycheck\\[s\\]]').val(Math.round(paycheck.s * 100) / 100, 2);
+        $('[name=paycheck\\[j\\]]').val(Math.round(paycheck.j * 100) / 100, 2);
     }
 
-    if (sum <= 0 || isNaN(sum)) {
-        error('Podaj sumę zarobioną przez zespół');
-    }
+};
 
-    var workdaysSum = 0;
-    $('[name^=workdays]').each(function(index, field) {
-        workdaysSum += parseInt($(field).val());
-    });
 
-    if (workdaysSum > 0) {
-        var rate = sum / workdaysSum;
 
-        $('#hourly').html(Math.round((rate / 8) * 100) / 100);
 
-        paycheck.a = rate * parseInt($('[name=workdays\\[a\\]').val());
-        paycheck.s = rate * parseInt($('[name=workdays\\[s\\]]').val());
-        paycheck.j = rate * parseInt($('[name=workdays\\[j\\]]').val());
-    }
 
-    // Urlop
-    paycheck.a += getHolidaysPaycheck(parseInt($('[name=holidays\\[a\\]]').val()));
-    paycheck.s += getHolidaysPaycheck(parseInt($('[name=holidays\\[s\\]]').val()));
-    paycheck.j += getHolidaysPaycheck(parseInt($('[name=holidays\\[j\\]]').val()));
 
-    // Soboty
-    paycheck.a += getWeekendPaycheck(parseInt($('[name=weekend\\[a\\]]').val()));
-    paycheck.s += getWeekendPaycheck(parseInt($('[name=weekend\\[s\\]]').val()));
-    paycheck.j += getWeekendPaycheck(parseInt($('[name=weekend\\[j\\]]').val()));
 
-    $('[name=paycheck\\[a\\]]').val(Math.round(paycheck.a * 100) / 100, 2);
-    $('[name=paycheck\\[s\\]]').val(Math.round(paycheck.s * 100) / 100, 2);
-    $('[name=paycheck\\[j\\]]').val(Math.round(paycheck.j * 100) / 100, 2);
-}
 
-$('button').click(function() {
-    error('');
-    $('#hourly').html('');
-    calculate();
-});
+
+
+
